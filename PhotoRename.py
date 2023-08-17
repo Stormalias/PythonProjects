516173d8f1abe8c44d819ab6bce1992c8ee2ca82
Praval Visvanath <pravalvis@gmail.com>
2023-08-17 16:08:19 +0800

    added basic renaming logic using os.rename

--- PhotoRename.py
+++ PhotoRename.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import os
+
+filePath = "C:/Users/User/Pictures/Test/"
+count = 0
+
+for file in os.listdir(filePath):
+    newName = str(count) + "Photo.jpg"
+    oldName = filePath + file
+    newName = filePath + newName
+    os.rename(oldName, newName)
+    count += 1
+
