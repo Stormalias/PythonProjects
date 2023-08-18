1299a4ad6134bdd6d470e5b9bd5b61a4cde00c5d
Praval Visvanath <pravalvis@gmail.com>
2023-08-18 23:22:54 +0800

    Added image from PIL

--- PhotoRename.py
+++ PhotoRename.py
@@ -1,12 +1,18 @@
 import os
+from PIL import Image
 
 filePath = "C:/Users/User/Pictures/Test/"
 count = 0
 
-for file in os.listdir(filePath):
-    newName = str(count) + "Photo.jpg"
-    oldName = filePath + file
-    newName = filePath + newName
-    os.rename(oldName, newName)
-    count += 1
+def renamer():
+    for file in os.listdir(filePath):
+        newName = str(count) + "Photo.jpg"
+        oldName = filePath + file
+        newName = filePath + newName
+        os.rename(oldName, newName)
+        count += 1
+
+
+with Image.open("C:/Users/The Weenus Machine/Pictures/4669fd1b60d9592b3191d226918cc766.png") as im:
+    im.show()
 
